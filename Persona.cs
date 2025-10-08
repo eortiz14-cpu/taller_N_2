0bf6c8044a29e41a4b3900fc8a6d1373c96db049
Edgar Ortiz <eortiz14@udi.edu.co>
2025-10-07 21:09:04 -0500

    versión 1

---
+++ Persona.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Persona
+{
+    public string Nombre { get; set; }
+    public double Peso { get; set; }  
+    public double Altura { get; set; } 
+
+    public int Edad { get; set; }
+    public string Genero { get; set; }
+    public string Documento { get; set; }
+    public string Direccion { get; set; }
+    public string Telefono { get; set; }
+
+    public Persona(string nombre, double peso, double altura, int edad, string genero, string documento, string direccion, string telefono)
+    {
+        Nombre = nombre;
+        Peso = peso;
+        Altura = altura;
+        Edad = edad;
+        Genero = genero;
+        Documento = documento;
+        Direccion = direccion;
+        Telefono = telefono;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("\n====== DATOS DE LA PERSONA =====");
+        Console.WriteLine($"su Nombre: {Nombre}");
+        Console.WriteLine($"su Edad: {Edad} años");
+        Console.WriteLine($"su Género: {Genero}");
+        Console.WriteLine($"su Documento: {Documento}");
+        Console.WriteLine($"su Dirección: {Direccion}");
+        Console.WriteLine($"su Teléfono: {Telefono}");
+        Console.WriteLine($"su Peso: {Peso} kg");
+        Console.WriteLine($"su Altura: {Altura} m");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese su nombre: ");
+        string nombre = Console.ReadLine() ?? "";
+
+        Console.Write("Ingrese su peso en kg: ");
+        double peso = Convert.ToDouble(Console.ReadLine() ?? "0");
+
+        Console.Write("Ingrese su altura en metros: ");
+        double altura = Convert.ToDouble(Console.ReadLine() ?? "0");
+
+        Console.Write("Ingrese su edad: ");
+        int edad = Convert.ToInt32(Console.ReadLine() ?? "0");
+
+        Console.Write("Ingrese su género: ");
+        string genero = Console.ReadLine() ?? "";
+
+        Console.Write("Ingrese su documento: ");
+        string documento = Console.ReadLine() ?? "";
+
+        Console.Write("Ingrese su dirección: ");
+        string direccion = Console.ReadLine() ?? "";
+
+        Console.Write("Ingrese su teléfono: ");
+        string telefono = Console.ReadLine() ?? "";
+
+        Persona persona = new Persona(nombre, peso, altura, edad, genero, documento, direccion, telefono);
+
+        persona.MostrarInformacion();
+    }
+}
